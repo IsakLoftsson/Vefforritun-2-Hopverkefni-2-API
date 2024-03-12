024d29c01092df739a1eadb255a9700aa7bee788
liljaork <liljaork@gmail.com>
2024-03-12 20:43:13 +0000

    pínu fínpúss á schema.sql og drop.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -6,21 +6,21 @@ CREATE TABLE public.users (
   admin BOOLEAN DEFAULT false
 );
 
-CREATE TABLE public.task_types (
-  id SERIAL PRIMARY KEY,
-  name CHARACTER VARYING(64) NOT NULL UNIQUE,
-  description TEXT DEFAULT ''
-);
-
-CREATE TABLE public.tasks (
+CREATE TABLE public.task (
   id SERIAL PRIMARY KEY,
   name CHARACTER VARYING(64) NOT NULL,
   date DATE NOT NULL DEFAULT CURRENT_DATE, -- Dagur en ekki tími
   user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE -- Einn user gerir hvert task
 );
 
-CREATE TABLE public.task_type_associations (
-  task_id INTEGER NOT NULL REFERENCES tasks(id) ON DELETE CASCADE,
-  type_id INTEGER NOT NULL REFERENCES task_types(id) ON DELETE CASCADE,
-  PRIMARY KEY (task_id, type_id)
+CREATE TABLE public.task_type (
+  id SERIAL PRIMARY KEY,
+  name CHARACTER VARYING(64) NOT NULL UNIQUE,
+  description TEXT DEFAULT ''
+);
+
+CREATE TABLE public.task_type_association (
+  task_id INTEGER NOT NULL REFERENCES task(id) ON DELETE CASCADE,
+  type_id INTEGER NOT NULL REFERENCES task_type(id) ON DELETE CASCADE,
+  PRIMARY KEY (task_id, type_id) -- tengir saman task og type og býr til primary Key fyrir viðburðinn
 );
