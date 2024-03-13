bdc880204b821fc57382b649530000ccb1910bef
liljaork <liljaork@gmail.com>
2024-03-13 21:21:50 +0000

    setti upp types.ts út frá schema töflunum

--- src/types.ts
+++ src/types.ts
@@ -1,39 +1,25 @@
-export type TeamStanding = {
-  position: number;
+export type User = {
+  id: number;
   name: string;
-  points: number;
+  username: string;
+  password: string;
+  admin: boolean;
 };
 
-export type Team = {
+export type Task = {
+  id: number;
+  name: string;
+  date: Date;
+  user_id: number; // Foreign key tengt user
+};
+
+export type TaskType = {
+  id: number;
   name: string;
   description?: string;
-  score: number;
+};
+export type TaskTypeAssociation = {
+  task_id: number; // Foreign key tengt task
+  type_id: number; // Foreign key tengt task_type
 };
 
-export type DatabaseTeam = {
-  id: string;
-  name: string;
-  slug: string;
-  description: string;
-};
-
-export type Game = {
-  id?: string;
-  date?: Date;
-  home: Team;
-  away: Team;
-};
-
-export type DatabaseGame = {
-  id: string;
-  date: string;
-  home_id: string;
-  away_id: string;
-  home_score: number;
-  away_score: number;
-};
-
-export type Gameday = {
-  date: Date;
-  games: Game[];
-};
