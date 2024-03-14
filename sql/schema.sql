ea2da9c5b31471fe3dff7eff622dce13cb069062
Ísak Loftsson <isak.l@bl.is>
2024-03-14 16:30:57 +0000

    breytti tímabundið schema og types til að einfalda fyrir okkur grunninn

--- sql/schema.sql
+++ sql/schema.sql
@@ -6,21 +6,31 @@ CREATE TABLE public.users (
   admin BOOLEAN DEFAULT false
 );
 
-CREATE TABLE public.task (
+CREATE TABLE public.task_types (
+  id SERIAL PRIMARY KEY,
+  name CHARACTER VARYING(64) NOT NULL UNIQUE
+);
+
+CREATE TABLE public.task_tags (
+  id SERIAL PRIMARY KEY,
+  name CHARACTER VARYING(64) NOT NULL UNIQUE
+);
+
+
+CREATE TABLE public.tasks (
   id SERIAL PRIMARY KEY,
   name CHARACTER VARYING(64) NOT NULL,
+  description CHARACTER VARYING(128) TEXT DEFAULT '',
   date DATE NOT NULL DEFAULT CURRENT_DATE, -- Dagsetning (gefur okkur ekki klukku)
+  task_type INTEGER NOT NULL REFERENCES task_type(id) ON DELETE CASCADE,
+  task_tag INTEGER NOT NULL REFERENCES task_tag(id) ON DELETE CASCADE, 
   user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE -- Einn user gerir hvert task
 );
 
-CREATE TABLE public.task_type (
-  id SERIAL PRIMARY KEY,
-  name CHARACTER VARYING(64) NOT NULL UNIQUE,
-  description TEXT DEFAULT ''
-);
-
+/* Bíðum aðeins með þetta
 CREATE TABLE public.task_type_association (
   task_id INTEGER NOT NULL REFERENCES task(id) ON DELETE CASCADE,
   type_id INTEGER NOT NULL REFERENCES task_type(id) ON DELETE CASCADE,
   PRIMARY KEY (task_id, type_id) -- tengir saman task og type og býr til primary Key fyrir viðburðinn
 );
+*/
--- src/types.ts
+++ src/types.ts
@@ -6,34 +6,49 @@ export type User = {
   admin: boolean;
 };
 
-export type Task = {
-  id: number;
-  name: string;
-  date: Date;
-  user_id: number; // Foreign key tengt user
-};
-
 export type TaskType = {
   id: number;
   name: string;
-  description?: string;
 };
 
+export type TaskTag = {
+  id: number;
+  name: string;
+};
+
+export type Task = {
+  id: number;
+  name: string;
+  description: string;
+  date: Date;
+  taskTypeId: TaskType;
+  taskTagId: TaskTag;
+  user_id: number; // Foreign key tengt user
+};
+
+/* Bíðum aðeins með þetta
 export type TaskTypeAssociation = {
   task_id: number; // Foreign key tengt task
   type_id: number; // Foreign key tengt task_type
+};
+*/
+
+export type DatabaseTaskType = {
+  id: string;
+  name: string;
+};
+
+export type DatabaseTaskTag = {
+  id: string;
+  name: string;
 };
 
 export type DatabaseTask = {
   id: string;
   name: string;
-  date: Date; 
-  user_id: string; // Foreign key tengt users
-};
-
-export type DatabaseTaskType = {
-  id: string;
-  name: string;
   description: string;
+  date: string;
+  task_type_id: string;
+  task_tag_id: string;
+  user_id: string;
 };
-
